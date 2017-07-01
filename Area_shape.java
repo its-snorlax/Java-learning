8e91e20e96c22df00f6c93b4e1ac4bff54a8b8cb
Prayas Jain <p.jain3322@gmail.com>
2017-07-01 12:58:10 +0530

    To find the area of triangle and square

---
+++ Area_shape.java
@@ -0,0 +1,45 @@
+abstract class Shape{
+	abstract void area();
+	abstract void disp();
+}
+
+class Triangle extends Shape{
+	int height, base;
+	float area;
+	public Triangle(int height, int base){
+		this.height = height;
+		this.base = base;
+	}
+	void area(){
+
+		area = (height * base)/2;
+
+	}
+	void disp(){
+		System.out.println(area);
+	}
+}
+
+class Square extends Shape{
+	int side;
+	float area;
+	public Square(int side){
+		side = side;
+	}
+	void area(){
+		area = side * side;
+	}
+	void disp(){
+		System.out.println(area);
+	}
+}
+public class Area_shape {
+	public static void main(String[] args) {
+		Triangle obj1 = new Triangle(2,4);
+		obj1.area();
+		obj1.disp();
+		Square obj2 = new Square(5);
+		obj2.area();
+		obj2.disp();
+	}
+}
